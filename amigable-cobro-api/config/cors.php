c0370b853ea62605fd4afcf6b370b1f1169d5c30
root <root@leonardo.localdomain>
2026-07-16 17:22:10 -0400

    feat: CORS_ALLOWED_ORIGINS y VITE_ALLOWED_HOSTS dinámicos por env + nuevo dominio cobro.amigable.app

--- amigable-cobro-api/config/cors.php
+++ amigable-cobro-api/config/cors.php
@@ -5,7 +5,7 @@ return [
     "allowed_methods" => ["*"],
     "allowed_origins" => env('APP_ENV') === 'local'
         ? ['*']
-        : ['https://amigablecobro.amacruxlab.com'],
+        : explode(',', env('CORS_ALLOWED_ORIGINS', 'https://amigablecobro.amacruxlab.com')),
     "allowed_origins_patterns" => [],
     "allowed_headers" => ["*"],
     "exposed_headers" => [],
